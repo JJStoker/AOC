12f3e3feeeb81e0a98d5b406f6f13f06d1b8fd3b
JJStoker <jeroen.stoker@dacom.nl>
2021-12-09 23:54:05 +0100

    Add day 04 part 2

--- 04/giant_squid.py
+++ 04/giant_squid.py
@@ -58,4 +58,44 @@ def part_1():
                             unmarked += card[x][y]
                 print(f'Part 1: {unmarked * number}')
 
+def part_2():
+    hits = [
+        [[False for _ in range(len(card[0]))] for _ in range(len(card))]
+        for card in bingo_cards
+    ]
+    finished_cards = [False for _ in range(len(bingo_cards))]
+    for number in bingo_numbers:
+        for (cI, card) in enumerate(bingo_cards):
+            won = False
+            range_x = range(len(card[0]))
+            range_y = range(len(card))
+            for x in range_x:
+                for y in range_y:
+                    if card[x][y] == number:
+                        hits[cI][x][y] = True
+            for x in range_x:
+                won_row = True
+                for y in range_y:
+                    if not hits[cI][x][y]:
+                        won_row = False
+                if won_row:
+                    won = True
+            for y in range_y:
+                won_col = True
+                for x in range_x:
+                    if not hits[cI][x][y]:
+                        won_col = False
+                if won_col:
+                    won = True
+            if won and not finished_cards[cI]:
+                finished_cards[cI] = True
+                if all([finished_cards[x] for x in range(len(bingo_cards))]):
+                    unmarked = 0
+                    for x in range_x:
+                        for y in range_y:
+                            if not hits[cI][x][y]:
+                                unmarked += card[x][y]
+                    print(f'Part 2: {unmarked * number}')
+
 part_1()
+part_2()
