54641537fc8589be166763d34c3cc4746b340295
JJStoker <jeroen.stoker@dacom.nl>
2021-12-09 18:52:25 +0100

    Add day 01 part 2

--- 01/sonar_sweep.py
+++ 01/sonar_sweep.py
@@ -16,4 +16,16 @@ def part_1():
         
     print(f'Part 1: {count}')
 
+
+def part_2():
+    count = 0
+    for (mI, m) in enumerate(rows):
+        if mI < 3: continue
+        prev_sum = rows[mI - 1] + rows[mI - 2] + rows[mI - 3]
+        curr_sum = m + rows[mI - 1] + rows[mI - 2]
+        if curr_sum > prev_sum:
+            count += 1
+    print(f'Part 2: {count}')
+
 part_1()
+part_2()
