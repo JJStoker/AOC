1c32c0c8f40485dc5b86619910404e2b179c338f
JJStoker <jeroen.stoker@dacom.nl>
2021-12-09 20:26:18 +0100

    Add day 03 part 2

--- 03/binary_diagnostics.py
+++ 03/binary_diagnostics.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import List, Tuple
+from typing import List
 
 INPUT_FILE = './sample.txt' if False else './input.txt'
 
@@ -23,4 +23,19 @@ def part_1():
         epsilon += f'{0 if ones[key] >= (byte_cnt / 2) else 1}'
     print (f'Part 1: {int(gamma, 2) * int(epsilon, 2)}')
 
+def part_2():
+    oxygen = list(byte_list)
+    co2_scrubber = list(byte_list)
+    for i in range(len(byte_list[0])):
+        if len(oxygen) > 1:
+            o0 = [x for x in oxygen if x[i] == '0']
+            o1 = [x for x in oxygen if x[i] == '1']
+            oxygen = o1 if len(o1) >= len(o0) else o0
+        if len(co2_scrubber) > 1:
+            o0 = [x for x in co2_scrubber if x[i] == '0']
+            o1 = [x for x in co2_scrubber if x[i] == '1']
+            co2_scrubber = o0 if len(o1) >= len(o0) else o1
+    print (f'Part 2: {int(oxygen[0], 2) * int(co2_scrubber[0], 2)}')
+
 part_1()
+part_2()
