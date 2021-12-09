6cdc44bd24662678170d31f46b24036064ca1dc6
JJStoker <jeroen.stoker@dacom.nl>
2021-12-10 00:53:32 +0100

    Add day 06 part 1

---
+++ 06/lanternfish.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from typing import List
+
+INPUT_FILE = './sample.txt' if False else './input.txt'
+
+
+fish: List[int] = []
+with open(INPUT_FILE, 'r') as input_file:
+    fish = [int(x) for x in input_file.read().split(',')]
+
+def part_1(days: int):
+    fish_state = defaultdict(int)
+    for f in fish:
+        fish_state[f] += 1    
+    for _ in range(days):
+        day_state = defaultdict(int)
+        for (cycle, count) in fish_state.items():
+            if cycle == 0:
+                day_state[6] += count
+                day_state[8] += count
+            else:
+                day_state[cycle - 1] += count
+        fish_state = day_state
+    print(f'Part 1: {sum(fish_state.values())}')
+
+part_1(80)
