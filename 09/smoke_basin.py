d8501c717e7f717163261fc393c55e0cb678bfe5
JJStoker <jeroen.stoker@dacom.nl>
2021-12-09 18:28:30 +0100

    Add day 09 part 2
    - cheated a little with finding out the right algorithm

--- 09/smoke_basin.py
+++ 09/smoke_basin.py
@@ -1,14 +1,16 @@
+from collections import deque
+from functools import reduce
 from typing import List
 
 INPUT_FILE = './sample.txt' if False else './input.txt'
 
 
+rows: List[List[int]] = [] 
+with open(INPUT_FILE, 'r') as input_file:
+    rows = list(map(lambda xs: list(map(int, xs)), map(list, map(str.strip, input_file.readlines()))))
+
 def part_1():
     score = 0
-    rows: List[List[int]] = [] 
-    with open(INPUT_FILE, 'r') as input_file:
-        rows = list(map(lambda xs: list(map(int, xs)), map(list, map(str.strip, input_file.readlines()))))
-        
     for (rI, row) in enumerate(rows):
         for (cI, col) in enumerate(row):
             if (
@@ -20,4 +22,36 @@ def part_1():
                 score += (col + 1)
     print(f'Part 1: {score}')
 
+
+def part_2():
+    row_dirs = [-1, 0, 1, 0]
+    col_dirs = [0, 1, 0, -1]
+    # Breadth-first search: https://en.wikipedia.org/wiki/Breadth-first_search
+    basin_sizes: List[int] = []
+    handled = set()
+    for rI in range(len(rows)):
+        for cI in range(len(rows[0])):
+            if (rI, cI) not in handled and rows[rI][cI] != 9:
+                size = 0
+                basin = deque()
+                basin.append((rI, cI))
+                while basin:
+                    (row, col) = basin.popleft()
+                    if (row, col) in handled:
+                        continue
+                    handled.add((row, col))
+                    size += 1
+                    for d in range(4):
+                        row_dir = row + row_dirs[d]
+                        col_dir = col + col_dirs[d]
+                        if (
+                            (0 <= row_dir < len(rows) - 1) and
+                            (0 <= col_dir < len(rows[0]) - 1) and
+                            rows[row_dir][col_dir] != 9
+                        ):
+                            basin.append((row_dir, col_dir))
+                basin_sizes.append(size)
+    print(f'Part 2: {reduce(lambda a, b: a * b, sorted(basin_sizes)[-3:])}')
+
 part_1()
+part_2()
