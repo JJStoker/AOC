c653ef4279bb565d2178c439de8d157b57a42191
JJStoker <jeroen.stoker@dacom.nl>
2021-12-10 21:29:29 +0100

    Add day 07 part 2

--- 07/threachery_of_whales.py
+++ 07/threachery_of_whales.py
@@ -1,8 +1,6 @@
-from collections import defaultdict
 from typing import List
 
 INPUT_FILE = './sample.txt' if False else './input.txt'
-
 
 crabs: List[int] = []
 with open(INPUT_FILE, 'r') as input_file:
@@ -12,4 +10,15 @@ def part_1():
     median = sorted(crabs)[len(crabs) // 2]
     print(f'Part 1: {sum(list(map(lambda x: abs(x - median), crabs)))}')
 
+def part_2():
+    scores = []
+    for median in range(max(crabs)):
+        score = 0
+        for pos in crabs:
+            distance = abs(pos - median)
+            score += (distance * (distance + 1) // 2)
+        scores.append(score)
+    print(f'Part 2: {min(scores)}')
+
 part_1()
+part_2()
