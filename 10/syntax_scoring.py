653c9cc546dceca6e215a87c716727895b14d384
JJStoker <jeroen.stoker@dacom.nl>
2021-12-10 20:06:26 +0100

    Add day 10 part 2

--- 10/syntax_scoring.py
+++ 10/syntax_scoring.py
@@ -1,6 +1,7 @@
-from typing import List, Tuple
-
 INPUT_FILE = './sample.txt' if False else './input.txt'
+
+open_chars  = ['{', '[', '<', '(']
+close_chars = ['}', ']', '>', ')']
 
 with open(INPUT_FILE, 'r') as input_file:
     navigation_log = list(map(lambda x: x.replace('\n', ''), input_file.readlines()))
@@ -8,8 +9,6 @@ with open(INPUT_FILE, 'r') as input_file:
 def part_1():
     score = 0
     score_map  = {')': 3, ']': 57, '}': 1197, '>': 25137}
-    open_chars  = ['{', '[', '<', '(']
-    close_chars = ['}', ']', '>', ')']
     for nav_log in navigation_log:
         unclosed = []
         for (i, c) in enumerate(list(nav_log)):
@@ -23,4 +22,28 @@ def part_1():
                     unclosed.pop()
     print(f'Part 1: {score}')
 
+def part_2():
+    scores = []
+    score_map  = {')': 1, ']': 2, '}': 3, '>': 4}
+    for nav_log in navigation_log:
+        score = 0
+        invalid = False
+        unclosed = []
+        for c in list(nav_log):
+            if invalid: continue
+            if c in open_chars:
+                unclosed.append(c)
+            elif c in close_chars:
+                if unclosed[-1] != open_chars[(close_chars.index(c))]:
+                    invalid = True
+                else:
+                    unclosed.pop()
+        if not invalid and len(unclosed):
+            for x in map(lambda x: close_chars[open_chars.index(x)], reversed(unclosed)):
+                score *= 5
+                score += score_map[x]
+            scores.append(score)
+    print (f'Part 2: {sorted(scores)[round(len(scores) // 2)]}')
+
 part_1()
+part_2()
