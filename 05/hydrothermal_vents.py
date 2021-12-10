c83dcda5b873b0891fc5b5c1dc947f210ca82af6
JJStoker <jeroen.stoker@dacom.nl>
2021-12-10 21:01:41 +0100

    Add day 05 part 2

--- 05/hydrothermal_vents.py
+++ 05/hydrothermal_vents.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 
-INPUT_FILE = './sample.txt' if False else './input.txt'
+INPUT_FILE = "./sample.txt" if False else "./input.txt"
 
 linesegments = []
 with open(INPUT_FILE, 'r') as input_file:
@@ -16,6 +16,17 @@ def part_1():
         for x in range(rx, rxx + 1):
             for y in range(ry, ryy + 1):
                 grid[(x, y)] += 1
-    print(f'Part 1: {len(list(filter(lambda x: x > 1, grid.values())))}')
+    print(f"Part 1: {len(list(filter(lambda x: x > 1, grid.values())))}")
+
+def part_2():
+    grid = defaultdict(int)
+    for [[x1, y1], [x2, y2]] in linesegments:
+        dx, dy = (x2 - x1), (y2 - y1)
+        for i in range(max(abs(dx), abs(dy)) + 1):
+            x = x1 + (0 if dx == 0 else 1 if dx > 0 else -1) * i
+            y = y1 + (0 if dy == 0 else 1 if dy > 0 else -1) * i
+            grid[(x, y)] += 1
+    print(f"Part 2: {len(list(filter(lambda x: x > 1, grid.values())))}")
 
 part_1()
+part_2()
