772f552fc4adf4529e40054ff908c2559a1b3b25
JJStoker <jeroen.stoker@dacom.nl>
2021-12-13 16:03:48 +0100

    Add day 13 part 2

--- 13/transparent_origami.py
+++ 13/transparent_origami.py
@@ -23,26 +23,27 @@ def initialize_grid():
         grid[coord] = '#'
 
 def get_max_y():
-    return max([c[1] for c in grid])
+    return max([c[1] for c in grid]) + 1
 
 def get_max_x():
-    return max([c[0] for c in grid])
+    return max([c[0] for c in grid]) + 1
 
 def fold_grid(d='x', n=0):
-    deleted = []
+    max_x = get_max_x()
+    max_y = get_max_y()
     if d == 'x':
-        for x in range(get_max_x() + 1):
+        for x in range(max_x):
             if x < n: continue
-            for y in range(get_max_y() + 1):
+            for y in range(max_y):
                 val = grid[(x, y)]
                 dx = (n - x) + n
                 if 0 <= dx and val == '#':
                     grid[(dx, y)] = '#'
                 del grid[(x, y)]
     else:
-        for y in range(get_max_y() + 1):
+        for y in range(max_y):
             if y < n: continue
-            for x in range(get_max_x() + 1):
+            for x in range(max_x):
                 val = grid[(x, y)]
                 dy = (n - y) + n
                 if 0 <= dy and val == '#':
@@ -51,17 +52,20 @@ def fold_grid(d='x', n=0):
     
 def dump_grid():
     os.system("clear")
-    for y in range(get_max_y() + 2):
-        print (''.join([grid[(x, y)] for x in range(get_max_x() + 2)]), end='\n')
+    for y in range(get_max_y()):
+        print (''.join([grid[(x, y)] for x in range(get_max_x())]), end='\n')
 
 def count_dots():
     return len(list(filter(lambda x: x == '#', grid.values())))
 
 def part_1():
     initialize_grid()
-    x_or_y, n = instructions[0]
-    fold_grid(d=x_or_y, n=n)
+    part_1 = None
+    for i, (x_or_y, n) in enumerate(instructions):
+        fold_grid(d=x_or_y, n=n)
+        if i == 0: part_1 = count_dots()
     dump_grid()
-    print(f'Part 1: {count_dots()}')
+    print(f'Part 1: {part_1}')
+    print(f'Part 2: {count_dots()}')
 
 part_1()
