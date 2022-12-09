31d995e2713d0b51c3c04b0f6c4f6e9c9eed9a63
JJStoker <jeroen.stoker@dacom.nl>
2022-12-09 09:56:05 +0100

    Add day 08 part 2

--- 2022/08/treetop_tree_house.py
+++ 2022/08/treetop_tree_house.py
@@ -1,4 +1,6 @@
-with open('input.txt' if False else 'sample.txt', 'r') as i:
+from functools import reduce
+
+with open('input.txt' if True else 'sample.txt', 'r') as i:
     rows = list(map(lambda l: l.replace("\n", ""), i.readlines()))
 
 def is_edge(x, y):
@@ -11,12 +13,48 @@ def is_visible(x, y):
     right = all(rows[x][y] > rows[i][y] for i in range(x + 1, len(rows)))
     return up or down or left or right
 
-def part_01():
+def scenic_score(x, y):
+    if is_edge(x, y): return 0
+    up, down, left, right = 0, 0, 0, 0
+    for t in reversed(rows[x][:y]):
+        if rows[x][y] > t:
+            up += 1
+        else:
+            up += 1
+            break
+    for t in rows[x][y + 1:]:
+        if rows[x][y] > t:
+            down += 1
+        else:
+            down += 1
+            break
+    for i in range(x - 1, -1, -1):
+        if rows[x][y] > rows[i][y]:
+            left += 1
+        else:
+            left += 1
+            break
+    for i in range(x + 1, len(rows)):
+        if rows[x][y] > rows[i][y]:
+            right += 1
+        else:
+            right += 1
+            break
+    return reduce(lambda a, b: a * b, [up, down, left, right])
+
+def get_visible_trees():
     visible = []
     for rx, row in enumerate(rows):
         for cx, col in enumerate(row):
             if is_edge(rx, cx) or is_visible(rx, cx):
                 visible.append((rx, cx))
-    print(f"Part 1: {len(visible)}")
+    return visible
+
+def part_01():
+    print(f"Part 1: {len(get_visible_trees())}")
+
+def part_02():
+    print (f"Part 2: {max(map(lambda tree: scenic_score(tree[0], tree[1]), get_visible_trees()))}")
 
 part_01()
+part_02()
