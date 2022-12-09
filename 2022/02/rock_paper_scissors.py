1a7c11170ed2a0d0dc0744a82aeecbc8bfbf4a9f
JJStoker <jeroen.stoker@dacom.nl>
2022-12-10 00:27:45 +0100

    Fix day 2 local variable override

--- 2022/02/rock_paper_scissors.py
+++ 2022/02/rock_paper_scissors.py
@@ -1,14 +1,16 @@
-with open('input_01.txt', 'r') as i:
-    rounds = map(lambda l: l.replace("\n", ""), i.readlines())
-
 points = dict(X=1, Y=2, Z=3, A=1, B=2, C=3)
+
+def get_rounds():
+    with open('input_01.txt', 'r') as i:
+        rounds = map(lambda l: l.replace("\n", ""), i.readlines())
+    return rounds
 
 def part_01():
     win = dict(X='C', Y='A', Z='B')
     tie = dict(X='A', Y='B', Z='C')
     lose = dict(X='B', Y='C', Z='A')
     score = 0
-    for opponent, my_move in map(lambda x: x.split(" "), rounds):
+    for opponent, my_move in map(lambda x: x.split(" "), get_rounds()):
         score += points[my_move]
         if win[my_move] == opponent:
             score += 6
@@ -22,7 +24,7 @@ def part_02():
     lose = dict(A='Z', B='X', C='Y')
     
     score = 0
-    for opponent, my_move in map(lambda x: x.split(" "), rounds):
+    for opponent, my_move in map(lambda x: x.split(" "), get_rounds()):
         if my_move == "Y":
             score += 3
             score += points[tie[opponent]]
@@ -32,7 +34,6 @@ def part_02():
             score += 6
             score += points[win[opponent]]
     print(score)
-
 
 part_01()
 part_02()
