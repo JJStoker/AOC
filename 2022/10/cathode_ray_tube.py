8a79a3f83b22fc6e8e14dea2a7cbc03a12015d9f
JJStoker <jeroen.stoker@dacom.nl>
2022-12-10 13:39:47 +0100

    Add day 10 part 2

--- 2022/10/cathode_ray_tube.py
+++ 2022/10/cathode_ray_tube.py
@@ -4,21 +4,41 @@ from collections import deque
 with open("input.txt" if True else "sample.txt", "r") as i:
     inputs = list(map(lambda l: l.replace("\n", ""), i.readlines()))
 
-def part_01():
+def solve():
+    grid = [[" "] * 40 for _ in range(6)]
+    
     signal = 0
     cycle = 0
     x = 1
-    for instruction in inputs:
-        cycle += 1
+    def render(x, cycle):
+        import os
+        os.system("clear")
+        if x - 1 <= cycle % 40 <= x + 1:
+            grid[cycle // 40][cycle % 40] = '#'
+        for i in range(6):
+            print(
+                "".join(
+                    [grid[i][y] for y in range(40)],
+                ),
+                end="\n",
+            )
+    
+    def update_signal(x, cycle):
         if cycle in [20, 60, 100, 140, 180, 220]:
-            signal += (cycle * x)
+            return (cycle * x)
+        return 0
+
+    for instruction in inputs:
+        render(x, cycle)
+        cycle += 1
+        signal += update_signal(x, cycle)
         cmd = instruction.split()
         if cmd[0] == "addx":
+            render(x, cycle)
             cycle += 1
-            if cycle in [20, 60, 100, 140, 180, 220]:
-                signal += (cycle * x)
+            signal += update_signal(x, cycle)
             x += int(cmd[1])
     
     print(f"Part 1: {signal}")
 
-part_01()
+solve()
