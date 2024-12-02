772f62ddeec8259a6c1b02460234b402d94d3a7b
JJStoker <jeroen229@gmail.com>
2024-12-02 17:28:30 +0100

    Day 1 - part 2

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,16 +1,35 @@
+import os
+
+DEBUG = os.getenv('DEBUG', 'off').lower() == 'on'
 
 
-def part_01(sample=False):
-    with open('sample.txt' if sample else 'input.txt', 'r') as i:
+def get_input():
+    items = []
+
+    with open('sample.txt' if DEBUG else 'input.txt', 'r') as i:
         items = map(
             lambda l: map(int, l.replace("\n", "").split('   ')),
             i.readlines()
         )
-    
-    a, b = list(zip(*items))
+    return list(items)
+
+def part_01():
+    a, b = list(zip(*get_input()))
     distances = []
     for x, y in zip(sorted(a), sorted(b)):
         distances.append(abs(x - y))
-    print (sum(distances))
+    print (F'Day 01, part 1: {sum(distances)}')
+
+def part_02():
+    a, b = list(zip(*get_input()))
+    similarity_score = []
+    
+    def similarity(n):
+        return n * len([i for i in b if i == n])
+    
+    for x in a:
+        similarity_score.append(similarity(x))
+    print (F'Day 01, part 2: {sum(similarity_score)}')
 
 part_01()
+part_02()
