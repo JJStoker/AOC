0194ef15b3befb04bf7a8e7b08691e7138dd864f
JJStoker <jeroen229@gmail.com>
2024-12-03 19:12:02 +0100

    Day 03 - part 2

--- 2024/03/main.py
+++ 2024/03/main.py
@@ -4,10 +4,10 @@ import re
 DEBUG = os.getenv("DEBUG", "off").lower() == "on"
 
 
-def get_input():
+def get_input(sample_filename=None):
     inputs = []
 
-    with open("sample.txt" if DEBUG else "input.txt", "r") as i:
+    with open(f"{sample_filename or "sample"}.txt" if DEBUG else "input.txt", "r") as i:
         inputs = map(
             lambda l: l.replace("\n", ""), i.readlines()
         )
@@ -23,7 +23,26 @@ def part_01():
     print(f"Day 03, part 1: {total}")
 
 def part_02():
-    pass
+    instructions = "".join(get_input("sample_2"))
+    
+    mul_regex = r"mul\((\d+),(\d+)\)"
+    do_regex = r"do\(\)"
+    dont_regex = r"don't\(\)"
+
+    total = 0
+    enabled = True
+    for i in re.split(f"({do_regex}|{dont_regex}|{mul_regex})", instructions):
+        if not i:
+            continue
+        if re.fullmatch(do_regex, i):
+            enabled = True
+        elif re.fullmatch(dont_regex, i):
+            enabled = False
+        elif enabled and (match := re.match(mul_regex, i)):
+            a, b = map(int, match.groups())
+            total += a * b
+
+    print(f"Day 03, part 2: {total}")
 
 part_01()
 part_02()
